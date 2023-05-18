4ab10d07b11126a5a5ead8d5d3cd5d6e957a4044
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-18 18:52:07 +0400

    Updated migrations for post table

--- database/migrations/2023_05_18_125218_create_posts_table.php
+++ database/migrations/2023_05_18_125218_create_posts_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
+            
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('body');
             $table->timestamps();
+            
+            
+            
         });
     }
 
